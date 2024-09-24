a89fa07db106b5f7fd2855e71864a92fb0b2f2a5
abdalrhman <radea4082@gmail.com>
2024-09-24 21:15:26 +0300

    updata 2

--- js/solarSystem.js
+++ js/solarSystem.js
@@ -1,6 +1,6 @@
 //Import
 import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
-import { OrbitControls } from "https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js";                                    
 //////////////////////////////////////
 //NOTE Creating renderer
 const renderer = new THREE.WebGLRenderer();
@@ -55,9 +55,9 @@ const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000
+  100000
 );
-camera.position.set(-50, 90, 150);
+camera.position.set(-70, 90, 180);
 ////////////////////////////////////
 
 //////////////////////////////////////
@@ -67,7 +67,7 @@ const orbit = new OrbitControls(camera, renderer.domElement);
 
 //////////////////////////////////////
 //NOTE - sun
-const sungeo = new THREE.SphereGeometry(15, 50, 50);
+const sungeo = new THREE.SphereGeometry(30, 50, 50);
 const sunMaterial = new THREE.MeshBasicMaterial({
   map: sunTexture,
 });
@@ -94,6 +94,8 @@ function createLineLoopWithMesh(radius, color, width) {
   const material = new THREE.LineBasicMaterial({
     color: color,
     linewidth: width,
+    opacity: 0.3, 
+    transparent: true
   });
   const geometry = new THREE.BufferGeometry();
   const lineLoopPoints = [];
@@ -117,7 +119,35 @@ function createLineLoopWithMesh(radius, color, width) {
 }
 //////////////////////////////////////
 
-/////////////////////////////////////
+
+////////////////////x-y from sun ////////////
+const planetDistances = {
+
+  mercury: 38, 
+  venus: 54,    
+  earth: 72,   
+  mars: 88,     
+  jupiter: 110,  
+  saturn: 148,
+  uranus: 186,   
+  neptune: 210,
+  pluto: 226     
+  
+};
+
+const planetSizes = {
+  mercury: 0.5,   
+  venus: 1.0,   
+  earth: 1.0,    
+  mars: 0.53,     
+  jupiter: 10.0, 
+  saturn: 8.0,
+  uranus: 4.0,   
+  neptune: 3.8,  
+  pluto: 0.7     
+};
+
+////////////////////////////////////////////
 //NOTE: create planet
 const genratePlanet = (size, planetTexture, x, ring) => {
   const planetGeometry = new THREE.SphereGeometry(size, 50, 50);
@@ -152,81 +182,164 @@ const genratePlanet = (size, planetTexture, x, ring) => {
   };
 };
 
+
+
+///////////////////////////real speed and defult speed ///////////
+
+
+
+const realSpeeds = {
+  mercuryAround: 0.00004,
+  venusAround: 0.00015,
+  earthAround: 0.00001,
+  marsAround: 0.00008,
+  jupiterAround: 0.00002,
+  saturnAround: 0.00009,
+  uranusAround: 0.00004,
+  neptuneAround: 0.00001,
+  plutoAround: 0.00007,
+
+  mercurySelf: 0.00004,
+  venusSelf: 0.00015,
+  earthSelf: 0.0001,
+  marsSelf: 0.0008,
+  jupiterSelf: 0.000002,
+  saturnSelf: 0.00009,
+  uranusSelf: 0.00004,
+  neptuneSelf: 0.00001,
+  plutoSelf: 0.00007,
+  
+};
+
+
+const defaultSpeeds = {
+  mercuryAround: 0.04,
+  venusAround: 0.015,
+  earthAround: 0.01,
+  marsAround: 0.008,
+  jupiterAround: 0.002,
+  saturnAround: 0.0009,
+  uranusAround: 0.0004,
+  neptuneAround: 0.0001,
+  plutoAround: 0.07,
+
+  mercurySelf: 0.04,
+  venusSelf: 0.015,
+  earthSelf: 0.01,
+  marsSelf: 0.008,
+  jupiterSelf: 0.00002,
+  saturnSelf: 0.0009,
+  uranusSelf: 0.0004,
+  neptuneSelf: 0.0001,
+  plutoSelf: 0.07,
+  sunSpeed:0.004
+
+};
+
+
+////////////////////////////////////////////////////
+
+
+
+
 const planets = [
   {
-    ...genratePlanet(3.2, mercuryTexture, 28),
-    rotaing_speed_around_sun: 0.004,
-    self_rotation_speed: 0.004,
+    ...genratePlanet(planetSizes.mercury, mercuryTexture, planetDistances.mercury),
+    rotaing_speed_around_sun: defaultSpeeds.mercuryAround,
+    self_rotation_speed: defaultSpeeds.mercurySelf,
   },
   {
-    ...genratePlanet(5.8, venusTexture, 44),
-    rotaing_speed_around_sun: 0.015,
-    self_rotation_speed: 0.002,
+    ...genratePlanet(planetSizes.venus, venusTexture, planetDistances.venus),
+    rotaing_speed_around_sun: defaultSpeeds.venusAround,
+    self_rotation_speed: defaultSpeeds.venusSelf,
   },
   {
-    ...genratePlanet(6, earthTexture, 62),
-    rotaing_speed_around_sun: 0.01,
-    self_rotation_speed: 0.02,
+    ...genratePlanet(planetSizes.earth, earthTexture, planetDistances.earth),
+    rotaing_speed_around_sun: defaultSpeeds.earthAround,
+    self_rotation_speed: defaultSpeeds.earthSelf,
   },
   {
-    ...genratePlanet(4, marsTexture, 78),
-    rotaing_speed_around_sun: 0.008,
-    self_rotation_speed: 0.018,
+    ...genratePlanet(planetSizes.mars, marsTexture, planetDistances.mars),
+    rotaing_speed_around_sun: defaultSpeeds.marsAround,
+    self_rotation_speed: defaultSpeeds.marsSelf,
   },
   {
-    ...genratePlanet(12, jupiterTexture, 100),
-    rotaing_speed_around_sun: 0.002,
-    self_rotation_speed: 0.04,
+    ...genratePlanet(planetSizes.jupiter, jupiterTexture, planetDistances.jupiter),
+    rotaing_speed_around_sun: defaultSpeeds.jupiterAround,
+    self_rotation_speed: defaultSpeeds.jupiterSelf,
   },
   {
-    ...genratePlanet(10, saturnTexture, 138, {
+    ...genratePlanet(planetSizes.saturn, saturnTexture, planetDistances.saturn, {
       innerRadius: 10,
       outerRadius: 20,
       ringmat: saturnRingTexture,
     }),
-    rotaing_speed_around_sun: 0.0009,
-    self_rotation_speed: 0.038,
+    rotaing_speed_around_sun: defaultSpeeds.saturnAround,
+    self_rotation_speed: defaultSpeeds.saturnSelf,
   },
   {
-    ...genratePlanet(7, uranusTexture, 176, {
+    ...genratePlanet(planetSizes.uranus, uranusTexture, planetDistances.uranus, {
       innerRadius: 7,
       outerRadius: 12,
       ringmat: uranusRingTexture,
     }),
-    rotaing_speed_around_sun: 0.0004,
-    self_rotation_speed: 0.03,
+    rotaing_speed_around_sun: defaultSpeeds.uranusAround,
+    self_rotation_speed: defaultSpeeds.uranusSelf,
   },
   {
-    ...genratePlanet(7, neptuneTexture, 200),
-    rotaing_speed_around_sun: 0.0001,
-    self_rotation_speed: 0.032,
+    ...genratePlanet(planetSizes.neptune, neptuneTexture, planetDistances.neptune),
+    rotaing_speed_around_sun: defaultSpeeds.neptuneAround,
+    self_rotation_speed: defaultSpeeds.neptuneSelf,
   },
   {
-    ...genratePlanet(2.8, plutoTexture, 216),
-    rotaing_speed_around_sun: 0.0007,
-    self_rotation_speed: 0.008,
+    ...genratePlanet(planetSizes.pluto, plutoTexture, planetDistances.pluto),
+    rotaing_speed_around_sun: defaultSpeeds.plutoAround,
+    self_rotation_speed: defaultSpeeds.plutoSelf,
   },
 ];
 
-//////////////////////////////////////
-
-//////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
 //NOTE - GUI options
 var GUI = dat.gui.GUI;
 const gui = new GUI();
 const options = {
   "Real view": true,
   "Show path": true,
+  "Real speed": false,
   speed: 1,
 };
 gui.add(options, "Real view").onChange((e) => {
   ambientLight.intensity = e ? 0 : 0.5;
 });
+
 gui.add(options, "Show path").onChange((e) => {
   path_of_planets.forEach((dpath) => {
     dpath.visible = e;
   });
 });
+
+
+gui.add(options, "Real speed").onChange((e) => {
+  updatePlanetSpeeds(e);
+  
+});
+
+function updatePlanetSpeeds(useRealSpeeds) {
+  planets.forEach((planet, index) => {
+    const planetNames = [
+      'mercuryAround', 'venusAround', 'earthAround', 
+      'marsAround', 'jupiterAround', 'saturnAround', 
+      'uranusAround', 'neptuneAround', 'plutoAround'
+    ];
+    
+    const speedSet = useRealSpeeds ? realSpeeds : defaultSpeeds;
+    planet.rotaing_speed_around_sun = speedSet[planetNames[index]];
+    planet.self_rotation_speed = speedSet[planetNames[index].replace('Around', 'Self')];
+
+  });
+}
+
+
 const maxSpeed = new URL(window.location.href).searchParams.get("ms")*1
 gui.add(options, "speed", 0, maxSpeed?maxSpeed:20);
 
@@ -235,7 +348,7 @@ gui.add(options, "speed", 0, maxSpeed?maxSpeed:20);
 //////////////////////////////////////
 //NOTE - animate function
 function animate(time) {
-  sun.rotateY(options.speed * 0.004);
+  sun.rotateY(options.speed *  0.00004 );
   planets.forEach(
     ({ planetObj, planet, rotaing_speed_around_sun, self_rotation_speed }) => {
       planetObj.rotateY(options.speed * rotaing_speed_around_sun);
