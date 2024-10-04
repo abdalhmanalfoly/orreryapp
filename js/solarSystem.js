437ba9638504309c62b7598747e93febe1a31163
Abdalrhman Foly <radea4082@gmail.com>
2024-10-04 20:22:45 +0300

    v--3

--- js/solarSystem.js
+++ js/solarSystem.js
@@ -1,6 +1,6 @@
 //Import
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
+import { OrbitControls } from "https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js";               
                      
 //////////////////////////////////////
 //NOTE Creating renderer
